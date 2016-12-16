6180834dd15dca7f834d0bdb4f3158bdcfd83b6b
yuyilei <yyl1352091742@gmail.com>
2016-12-17 00:08:46 +0800

    pat

--- 1054.py
+++ 1054.py
@@ -2,37 +2,53 @@
 import string
 
 nums = string.digits + '.' + '-'
-n = raw_input() 
-m = int(n)
-c = [] 
+n = input() 
+b = raw_input() 
 i = 0 
-for i in range(m) :
-    a = raw_input()
-    c.append(a)
+c = b.split()
 
 sumall = 0 
 count =  0 
-for each in c :
-    eachchar = str(each)
-    try :
-        eachint  = int(each)
-    except :
-        pass 
-    if eachchar not in nums :
-        print 'ERROR:',eachchar ,'is not a legal number'
-    else :
-        if '.' in eachchar :
-            if '.' not in eachchar[-4:-1] :
-                print 'ERROR:',eachchar ,'is not a legal number '
+for each in c : 
+    for eachchar in each :
+        if eachchar not in nums :
+        print  'ERROR:',each ,'is not a legal number'
+        each = ' '
+        else :
+            pass 
+    if each != ' ' and '.' in each :
+        count1 = 0 
+        for k in each :
+            if k == '.' :
+                count1 = count1 + 1
             else  :
-                count = count + 1
-                sumall = sumall + eachint 
+                pass 
+            if count1 > 1 :
+                print  'ERROR:',each,'is not a legal number'
+            else :
+                if '.' not in each[-3:] and len(each) > 3 :
+                    print 'ERROR:',each,'is not a legal number'
+                elif each[0] == '.' and len(each) == 3 :
+                    print 'ERROR:',each,'is not a legal number'
+                else :
+                    eachfloat = float(each) 
+                    if eachfloat > 1000.0 or eachfloat < -1000.0 :
+                        print 'ERROR:',each,'is not a legal number'
+                    else:
+                        sumall = sumall + eachfloat 
+                        count = count + 1
+    elif each != ' ' and '.' not in each :
+        eachfloat = float(each) 
+        if eachfloat > 1000.0 or eachfloat < -1000.0 :
+            print 'ERROR:',each,'is not a legal number'
+        else :
+            sumall = sumall + eachfloat 
+            count = count + 1
 
 if count == 0:
     print 'The average of 1 number is Undefined'
 else :
-    count2 = float(count) 
-    res = round(sumall/count2 ,2)
+    res = round(sumall/count,2)
     print 'The average of',count, 'number is ', res
 
     
