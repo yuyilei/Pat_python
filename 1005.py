a6cd5dc9dc3bec53ad852e5a68f29ecff5a2c8ff
yuyilei <yyl1352091742@gmail.com>
2016-12-17 21:27:00 +0800

    pat

--- 1005.py
+++ 1005.py
@@ -1,8 +1,8 @@
 n = input() 
 b = raw_input()
 k = b.split()
-a = [ int(i) for i in k ]
-res = a
+a = [int(i) for i in k]
+res = [int(i) for i in k]  
 for c in a :
     while c != 1 :
         if c % 2 == 0 :
@@ -18,7 +18,5 @@ for c in a :
             else :
                 pass 
 res.sort(reverse = True)
-
-lens = len(res)
-for i in range(lens) :
-    print res[i],
+for i in res :
+    print i,
