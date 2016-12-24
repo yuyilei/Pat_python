84b40d297dd7c862cc4bc4cb5858f237f3a1638c
yuyilei <yyl1352091742@gmail.com>
2016-12-24 20:36:58 +0800

    pat

---
+++ 1034-2.py
@@ -0,0 +1,62 @@
+from fractions import *
+def add_brackets(n) :
+    if '-' in n :
+        _res  = '(' + n + ')'
+    else :
+        _res = n 
+    return _res 
+
+def change(n) :
+    fraction = Fraction(n) 
+    numerator = abs(fraction.numerator) 
+    denominator =abs(fraction.denominator)
+    if numerator == 0:
+            res_ ='0'
+    else :
+        if denominator == 1 :
+            res = str(numerator) 
+        elif numerator > denominator and denominator != 1 :
+            a = numerator / denominator 
+            b = numerator % denominator 
+            res = str(a) +' ' + str(b)+'/'+str(denominator)
+    if '-' in n and numerator != 0 :
+        res_ = '('+'-'+ res+ ')'
+    return  res_  
+
+def add_2(a,b) :
+    res_ = Fraction(a) + Fraction(b) 
+    res = change(str(res_)) 
+    return res 
+
+def subtraction_2(a,b) :
+    res_ = Fraction(a) - Fraction(b)
+    res = change(str(res_)) 
+    return res 
+
+def multi_2(a,b) :
+    res_ = Fraction(a) * Fraction(b) 
+    res = change(str(res_))
+    return res 
+
+def divsion_2(a,b) :
+    if '0' in b :
+        res = 'Inf' 
+    else :
+        res_ = Fraction(a) / Fraction(b) 
+        res = change(str(res_))
+    return res 
+
+if __name__ == '__main__' :
+    inputs = raw_input()
+    inputs_ = inputs.split()
+    former = inputs_[0]
+    later = inputs_[1]
+    real_former = change(former) 
+    real_later = change(later) 
+    print real_former,'+',real_later,'=',add_2(former,later) 
+    print real_former,'-',real_later,'=',subtraction_2(former,later)
+    print real_former,'*',real_later,'=',multi_2(former,later)
+    print real_former,'/',real_later,'=',divsion_2(former,later) 
+
+    
+    
