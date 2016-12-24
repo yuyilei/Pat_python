c523c6057b336212295986eb6b059275a7e5e6fe
yuyilei <yyl1352091742@gmail.com>
2016-12-24 17:05:36 +0800

    pat

--- 1019.py
+++ 1019.py
@@ -1,3 +1,23 @@
+def add_to_4(n) :
+    if len(n) == 4 :
+        res = n
+    else :
+        res_ = [ i for i in n ]
+        for i in range(4-len(n)) :
+            res_.append('0')
+        res = ''.join(res_) 
+    return res
+
+def former_add_to_4(n) :
+    if len(n) == 4 :
+        res = n
+    else :
+        a = []
+        for i in range( 4 -len(n)) :
+            a.append('0')
+            _res = ''.join(a)
+        res = _res + n 
+    return res 
 
 def tell_0(n) :
     if n[0] == n[1] == n[2] == n[3] :
@@ -6,12 +26,14 @@ def tell_0(n) :
         return True 
 
 
-def sort(n) :
-    number = [ i for i in n ]
+def sort_(n) :
+    number = [ int(i) for i in n ]
     a = sorted(number)
     b = sorted(number,reverse = True )
-    former = ''.join(b) 
-    later = ''.join(a) 
+    c = [str(i) for  i in a ]
+    d = [str(i) for  i in b ]
+    former = ''.join(d) 
+    later = ''.join(c) 
     return (former , later)
 
 def subtraction(a,b) :
@@ -19,21 +41,22 @@ def subtraction(a,b) :
     later = int(b)
     res = former - later 
     res2 = str(res)
-    return (res,res2)
+    return res2
 
 if __name__ == '__main__' :
-    n = raw_input()
+    s = raw_input()
+    n = add_to_4(s)
     if tell_0(n) == False :
         print ('%s - %s = 0000' % (n,n))
     else :
         while True :
-            sort = sort(n)
-            subtraction = subtraction(sort[0],sort[1])
-            print sort[0],'-',sort[1],'=',subtraction[0]
-            if subtraction[1] == '6174' :
+            former , later = sort_(n)
+            res = subtraction(former,later)
+            print former ,'-',later,'=',former_add_to_4(res)
+            if res == '6174' :
                 break
             else :
-                n = subtraction[1]
+                n = add_to_4(res) 
 
 
     
