48871514c83769a2853a7f607cf8ba965fcf9449
yuyilei <yyl1352091742@gmail.com>
2016-12-25 18:26:03 +0800

    pat

---
+++ 1015.py
@@ -0,0 +1,37 @@
+
+def to_sort_sum(n) : 
+    for i in range(n) : 
+        n[i].append(n[i][1]+[i][2]) 
+    for 
+        
+
+            
+
+
+        
+
+if __name__ == '__main__' :
+    inputs = raw_input()
+    inouts_ = inputs.split() 
+    number = int(inputs_) 
+    low = int(inputs_) 
+    high = int(inputs_) 
+    first = []
+    second = [] 
+    third = [] 
+    fourth = [] 
+
+    for i in range(n) :
+        student = raw_input() 
+        student_ = [int(i) for i in student.split()] 
+        moral = student_[1] 
+        ability = student_[2]
+        if moral >= low and  ability >= low :
+            if moral >= high and ability >= high :
+                first.append(student_) 
+            elif moral >= high and  abliity < high :
+                second.append(student_) 
+            elif abliity <= moral < high :
+                third.append(student_) 
+            else :
+                fourth.append(student_) 
