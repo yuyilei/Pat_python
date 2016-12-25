fd0a591576e16a038a00d05c33533ae84dc6a450
yuyilei <yyl1352091742@gmail.com>
2016-12-25 20:02:44 +0800

    pat

--- 1058.py
+++ 1058.py
@@ -1,12 +1,25 @@
 
-def to_find_brackets(n) :
+def to_remove_brackets(n) :
     list_n = [ i for i in n ]
-    index = []
-    for each in list_n :
-        if each == '(' or each == ')' :
-            index.append(list_n.index(each))
+    res = []
+    flag1 = []
+    flag2 = [] 
+    for i in range(len(n)) : 
+        if list_n[i] == '(' :
+            flag1.append(i) 
+        if list_n[i] == ')' :
+            flag2.append(i) 
+    for i , j in zip(flag1,flag2) :
+        res.append(''.join(list_n[i+2:j]))  
+    return res 
 
-    return index
+def handle_answer(n) :
+    list_n = [ i for i in n ]
+    number = n[:3] 
+    real_number = [int(i) for i in number ]
+    res = real_number.append(''.join(list_n[3:]))
+    return res
+    
 
 if __name__ == '__main__' :
     inputs = raw_input()
@@ -14,13 +27,43 @@ if __name__ == '__main__' :
     number = int(inputs_[0])
     problem = int(inputs_[1])
 
-    answers = []
+    answer = []
     students = []
+    scores = [ 0 for i in range(number)] 
+    error = []
 
     for i in range(problem) :
-        answers.append(raw_input())
+        answer.append(handle_answer(raw_input().split()))
     for i in range(number) :
-        students.append(raw_input())
+        students.append(to_remove_brackets(raw_input().split())) 
+
+    max_error = 0
+
+    for each in answer :
+        index = answer.index(each) 
+        errors = 0 
+        for each_st in students :
+            index_st = students.index(each_st)
+            if each_st[index] == each[3] :
+                scores[index_st] += each[0] 
+            else :
+                errors += 1
+        if errors >= max_error :
+            max_error = errors 
+            error.append(index) 
+
+    for each in scores :
+        print each 
+    if max_error == 0 :
+        print 'Too simple'
+    else :
+        for i in len(error) :
+            error.insert(0,max_error) 
+        for i in error :
+            print i , 
+
+
+        
 
 
      
