736f206d7bf3d97adec87c282dd1df16ad125792
yuyilei <yyl1352091742@gmail.com>
2016-12-31 23:56:39 +0800

    pat

--- 1054-2.py
+++ 1054-2.py
@@ -2,14 +2,69 @@
 import string 
 nums = string.digits + '-' +'.'
 
+
+def tell_digit(n) :
+
+    real_n = float(n)
+    list_n = [ i for i in n]
+    lens = len(list_n)
+    try :
+        index_ = list_n.index('-')
+    except:
+        index_ = -1
+        pass
+
+    try :
+        index_1 = list_n.index('.') 
+    except :
+        index_1 = 0 
+        pass 
+    if real_n == 0 :
+        if '-' in list_n and lens == 2 :
+            return True 
+        elif '-' not in list_n and lens == 1 :
+            return True 
+        else :
+            return False 
+    else :
+        if '.' not in list_n :
+            if list_n[index_+1] == '0' :
+                return False
+            else :
+                return True 
+        else :
+            if  -1 < abs(real_n) < 1 :
+                if  '-' in list_n and index_1 == 2 :
+                    return True 
+                elif '-' not in list_n and index_1 == 1  :
+                    return True 
+                else :
+                    return False
+            else :
+                if list_n[index_+1] != '0' and '-' in n :
+                    return True 
+                elif list_n[index_+1] != '0' and '-' not in n  :
+                    return True
+                else :
+                    return False 
+
+
+                    
+
+
+            
+                
+
 def tell_char(n) :
     tell = 0
     for i in n :
         if i not in nums :
             tell = tell + 1
-            return False
+            break 
     if tell == 0 :
-        return True 
+        return True
+    else :
+        return False
 
 def tell_border(n) :
     if '.' in n :
@@ -37,33 +92,32 @@ def tell_minus(n) :
 
 
 def tell_point(n) :
-    if '.' in n :
-        lens = len(n)
-        char = [ i for  i in n ]
-        if  char[lens - 1] == '.' :
+     if '.' in n :
+         lens = len(n)
+         char = [ i for  i in n ]
+         if  char[lens - 1] == '.' :
             return False
-        else :
-            
-            if char.count('.') > 1 :
-                return False 
-            else :
-                if lens <= 2 :
+         else :
+             if char.count('.') > 1 :
+                 return False 
+             else :
+                 if lens <= 2 :
+                     return False
+                 elif lens == 3 and char[1] != '.' and '-' not in char :
                     return False
-                elif lens == 3 and char[1] != '.' and '-' not in n :
+                 elif lens == 3 and '-' in char :
                     return False
-                elif lens == 3 and '-' in n :
-                    return False
-                elif lens == 4 and '-' in n and char[2] != '.' :
+                 elif lens == 4 and '-' in char and char[2] != '.' :
                     return False 
-                elif lens == 4 and '-' not in n and '.' not in char[-3:] :
+                 elif lens == 4 and '-' not in char and '.' not in char[-3:-1] :
                     return False
-                elif lens > 4 and '.' not in char[-3:]   :
+                 elif lens > 4 and '.' not in char[-3:-1] :
                     return False
-                else :
+                 else :
                     return True
 
-    else :
-        return True 
+     else :
+         return True 
             
 
 if __name__ == '__main__':
@@ -85,15 +139,17 @@ if __name__ == '__main__':
                     if tell_border(n) == False :
                         print ('ERROR: %s is not a legal number' % n )
                     else :
-                        if '.' in n :
+                        if tell_digit(n) == False :
+                            print ('ERROR: %s is not a legal number' % n)
+                        else :
                             sumall = sumall + float(n)
                             count = count + 1
-                        else :
-                            sumall = sumall + int(n)
-                            count = count + 1
+                    
 
 if count == 0 :
-    print ("The average of 0 numbers is Undefined")
+    print ("The average of 0 numbers is Undefined" )
+elif count == 1 :
+    print ("The average of 1 number is %.2f" % (sumall ))
 else :
     print ('The average of %d numbers is %.2f' % (count ,sumall/count)) 
 
