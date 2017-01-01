c58133f7c86a578dff12630859fab8b2b925be49
yuyilei <yyl1352091742@gmail.com>
2017-01-01 22:41:00 +0800

    pat

---
+++ 1014.py
@@ -0,0 +1,56 @@
+
+def to_find_weekday(n,m) :
+    al = 'ABCDEFG'
+    day = { 'A' : 'MON' , 'B' : 'TUE' ,'C' :'WED' ,'D' :'THU','E' : 'FRI' , 'F'
+            :'SAT' , 'G' : 'SUN' } 
+    for i in n :
+        if i in al :
+            index = n.index(i) 
+            if m[index] == i :
+                key = i 
+                break
+    return day[key] , index 
+
+def to_find_hour(n,m) :
+    al_ = '0123456789ABCDEFGHIJKLMN' 
+    hour = { '0' : '00' ,'1' :'01' , '2' :'02' , '3' :'03' , '4' : '04' , '5'
+            :'05' , '6' : '06' , '7' : '07' , '8' : '08' , '9' :'09' , 'A' :'10'
+            , 'B' : '11' , 'C' :'12','D' :'13', 'E' : '14','F' :'15','G'
+            :'16','H' :'17','I':'18','J':'19','K':'20','L':'21','M':'22','N':'23'} 
+    lens = len(n) 
+    index = to_find_weekday(n,m)[1]
+
+    for i in range(index+1,lens) :
+        if n[i] in al_ and n[i] == m[i] :
+            key = n[i] 
+            break 
+    return hour[key]
+
+import string 
+alphas = string.letters.lower() 
+
+def to_find_minute(n,m) :
+    for i , j in n ,m :
+        if i in alphas and j in alphas and i == j :
+            index = n.index(i)
+            break
+    return index 
+
+
+if __name__ == '__main__' :
+    one = raw_input()
+    two = raw_input()
+    three = raw_input()
+    four = raw_input()
+
+    day = to_find_weekday(one,two)[0]
+    hour = to_find_hour(one,two)
+    minute = str(to_find_minute(three,four))
+    
+    if lens(minute) == 1 :
+        mintue = '0' + minute 
+
+    res  = day + ':' + hour + ':' + minute
+
+    print res 
+
