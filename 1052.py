75b739ca7dcbd95a211314fbe403727c7122c22f
yuyilei <yyl1352091742@gmail.com>
2017-01-10 20:57:15 +0800

    pat

---
+++ 1052.py
@@ -0,0 +1,34 @@
+
+
+def remove(n) :
+    a = [ i for i in n.split('[') ]
+    b = ''.join(a)
+    c = [ i for i in b.split(']') ]
+    for i in c :
+        if i == '' or i == ' ' :
+            del i
+    return c
+
+
+
+if __name__ == '__main__' :
+    hands = input()
+    eyes = input()
+    mouth = input() 
+    real_hands = remove(hands)
+    real_eyes = remove(eyes)
+    real_mouth = remove(mouth)
+    n =  int(input())
+    real_res = [] 
+    for i in range(n) :
+        number = input().split()
+        real_n = [ int(i) for i in number ]
+        try :
+            output = \
+            real_hands[real_n[0]-1]+'('+real_eyes[real_n[1]-1]+real_mouth[real_n[2]-1]+real_eyes[real_n[3]-1]+')'+real_hands[real_n[4]-1]
+        except :
+            output = 'Are you kidding me? @\/@'
+        real_res.append(output) 
+
+    for each in real_res :
+        print(each) 
