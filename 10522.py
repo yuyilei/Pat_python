b38a92ceb419139f274637e9b9c6d130fc83ff98
yuyilei <yyl1352091742@gmail.com>
2017-01-12 00:13:38 +0800

    pat

--- 10522.py
+++ 10522.py
@@ -20,15 +20,19 @@ if __name__ == '__main__' :
     real_hands = remove(hands)
     real_eyes = remove(eyes)
     real_mouth = remove(mouth)
+    len_h = len(real_hands) 
+    len_e = len(real_eyes)
+    len_m = len(real_mouth)
     n =  int(input())
     real_res = [] 
     for i in range(n) :
         number = raw_input().split()
         real_n = [ int(i) for i in number ]
-        try :
+        if 1<=real_n[0]<=len_h and 1<=real_n[1]<=len_e and 1<=real_n[2]<=len_m and \
+                1<=real_n[3]<=len_e and 1<=real_n[4]<=len_h :
             output = \
             real_hands[real_n[0]-1]+'('+real_eyes[real_n[1]-1]+real_mouth[real_n[2]-1]+real_eyes[real_n[3]-1]+')'+real_hands[real_n[4]-1]
-        except IndexError:
+        else :
             output = 'Are you kidding me? @\/@'
         real_res.append(output) 
 
