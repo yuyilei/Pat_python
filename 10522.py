8ca0cfd6372a5483f6d55a841f5f27a2fa6e1146
yuyilei <yyl1352091742@gmail.com>
2017-01-13 08:57:17 +0800

    pat

--- 10522.py
+++ 10522.py
@@ -1,12 +1,5 @@
 
 
-def cheak(n) :
-    number = min(n.count("["),n.count("]"))
-    res = []
-    i = 0 
-    while number > 0 :
-        if n[i] == "[" :
-
 
 def remove(n) :
     a = [ i for i in n.split('[') ]
